a86f2f22a2343d72cdea3f97e1298002e03e74a4
SubjectQuit <103890959+SubjectQuit@users.noreply.github.com>
2024-03-10 18:31:02 +0100

    Added AboutActivity and Button

--- app/src/main/java/com/example/unbuttoned/MainActivity.java
+++ app/src/main/java/com/example/unbuttoned/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.unbuttoned;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
         setScore(0);
         press.setOnClickListener(v -> incrementScore());
+
+        final FloatingActionButton infoButton = findViewById(R.id.info_btn);
+        infoButton.setOnClickListener(v -> startActivity(new Intent(this, AboutActivity.class)));
 
     }
 
